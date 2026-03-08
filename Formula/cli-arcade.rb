0625480c3c7b1646483e32dd8db1fe05bcd107a8
oilater <oilater@naver.com>
2026-03-08 13:11:19 +0900

    chore: v0.1.3 릴리스 반영 (darwin-arm64, linux-x64)

--- Formula/cli-arcade.rb
+++ Formula/cli-arcade.rb
@@ -1,30 +1,23 @@
 class CliArcade < Formula
   desc "Terminal arcade game"
   homepage "https://github.com/oilater/cli-arcade"
-  version "0.1.0"
+  version "0.1.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/oilater/cli-arcade/releases/download/v0.1.0/ca-darwin-arm64"
-      sha256 "ce2920188a566d496b831712f104d043d1b6230b536442b3c74ea9b7f38e641c"
+      url "https://github.com/oilater/cli-arcade/releases/download/v0.1.3/ca-darwin-arm64"
+      sha256 "b7a275001c95597a07ff8c7b026c0dbd453a995f665bb8d6ca78256553047d98"
 
       def install
         bin.install "ca-darwin-arm64" => "ca"
-      end
-    elsif Hardware::CPU.intel?
-      url "https://github.com/oilater/cli-arcade/releases/download/v0.1.0/ca-darwin-x64"
-      sha256 "PLACEHOLDER"
-
-      def install
-        bin.install "ca-darwin-x64" => "ca"
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/oilater/cli-arcade/releases/download/v0.1.0/ca-linux-x64"
-      sha256 "PLACEHOLDER"
+      url "https://github.com/oilater/cli-arcade/releases/download/v0.1.3/ca-linux-x64"
+      sha256 "76544173a979262ab0f15320c8dc5886e7380a0d73c55dcf5324dc629a036838"
 
       def install
         bin.install "ca-linux-x64" => "ca"
