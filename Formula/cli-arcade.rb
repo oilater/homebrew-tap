5f4c74dc739882d4a4e9bc14ec3a6a0e82ddb4c3
oilater <oilater@naver.com>
2026-03-08 10:41:35 +0900

    feat: add darwin-x64 and linux-x64 support (placeholder)

--- Formula/cli-arcade.rb
+++ Formula/cli-arcade.rb
@@ -7,11 +7,29 @@ class CliArcade < Formula
     if Hardware::CPU.arm?
       url "https://github.com/oilater/cli-arcade/releases/download/v0.1.0/ca-darwin-arm64"
       sha256 "ce2920188a566d496b831712f104d043d1b6230b536442b3c74ea9b7f38e641c"
+
+      def install
+        bin.install "ca-darwin-arm64" => "ca"
+      end
+    elsif Hardware::CPU.intel?
+      url "https://github.com/oilater/cli-arcade/releases/download/v0.1.0/ca-darwin-x64"
+      sha256 "PLACEHOLDER"
+
+      def install
+        bin.install "ca-darwin-x64" => "ca"
+      end
     end
   end
 
-  def install
-    bin.install "ca-darwin-arm64" => "ca"
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/oilater/cli-arcade/releases/download/v0.1.0/ca-linux-x64"
+      sha256 "PLACEHOLDER"
+
+      def install
+        bin.install "ca-linux-x64" => "ca"
+      end
+    end
   end
 
   test do
